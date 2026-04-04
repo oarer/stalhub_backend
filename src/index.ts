aeca615a6d15533f2a90538616cd1481a5aeded8
oarer <oarer.yml@gmail.com>
2026-04-04 22:01:27 +0500

    update root route

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,12 @@ import { createElysia } from './utils/elysia'
 const server = createElysia()
   // Routes
   .use(app)
+  
+  .get(
+    "/",
+    () =>
+      `Hi!\nThis API was created for https://stalhub.tech\n\nBy: https://github.com/oarer`
+  )
 
 server.listen({ port: env.PORT }, ({ hostname, port }) => {
   const url = env.NODE_ENV === 'production' ? 'https' : 'http'
