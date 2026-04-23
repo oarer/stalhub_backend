182ac3097a43694564768a3d6367f39e76d4fc92
oarer <oarer.yml@gmail.com>
2026-04-23 20:09:50 +0500

    remove unused types

--- src/types/arsenal.type.ts
+++ src/types/arsenal.type.ts
@@ -7,16 +7,6 @@ export type ItemInput = {
 export type ItemResult = ItemInput & {
 	currentPrice: number | null
 }
-//! MOVE TO Api TYPES
-export type AuctionHistoryResponse = {
-	total: number
-	prices: {
-		amount: number
-		price: number
-		time: string
-		additional?: Record<string, unknown>
-	}[]
-}
 
 export type CacheData = {
 	items: ItemResult[]
