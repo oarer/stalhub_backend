f75a347abdae02cb1b71ddeb4f47558ec1babdc3
oarer <oarer.yml@gmail.com>
2026-04-24 16:46:19 +0500

    fix: change create elysia route

--- src/app/api/auction/lots/index.ts
+++ src/app/api/auction/lots/index.ts
@@ -1,46 +1,47 @@
 import type { AxiosError } from 'axios'
-import { Elysia, t } from 'elysia'
+import { t } from 'elysia'
 import { apiClient } from '@/app/interceptors/sc.interceptor'
 import { type LotsResponse, Regions } from '@/types/api.type'
+import { createElysia } from '@/utils/elysia'
 
-export const lotsRoute = new Elysia().get(
-    '/:region/:id/lots',
-    async ({ params, query, set }) => {
-        const { id, region } = params
+export const lotsRoute = createElysia()
+	.get('/:region/:id/lots',
+		async ({ params, query, set }) => {
+			const { id, region } = params
 
-        const limit = query.limit ?? '10'
-        const additional = query.additional ?? 'true'
+			const limit = query.limit ?? '10'
+			const additional = query.additional ?? 'true'
 
-        try {
-            const { data } = await apiClient.get<LotsResponse>(
-                `/${region}/auction/${id}/lots`,
-                {
-                    params: { limit, additional },
-                }
-            )
+			try {
+				const { data } = await apiClient.get<LotsResponse>(
+					`/${region}/auction/${id}/lots`,
+					{
+						params: { limit, additional },
+					}
+				)
 
-            return data
-        } catch (err) {
-            const error = err as AxiosError<{ message?: string }>
-            const status = error.response?.status ?? 500
-            const message =
-                error.response?.data?.message ??
-                error.message ??
-                'Unknown error'
+				return data
+			} catch (err) {
+				const error = err as AxiosError<{ message?: string }>
+				const status = error.response?.status ?? 500
+				const message =
+					error.response?.data?.message ??
+					error.message ??
+					'Unknown error'
 
-            set.status = status
+				set.status = status
 
-            return { error: message }
-        }
-    },
-    {
-        params: t.Object({
-            id: t.String(),
-            region: t.Enum(Regions),
-        }),
-        query: t.Object({
-            limit: t.Optional(t.String()),
-            additional: t.Optional(t.String()),
-        }),
-    }
-)
+				return { error: message }
+			}
+		},
+		{
+			params: t.Object({
+				id: t.String(),
+				region: t.Enum(Regions),
+			}),
+			query: t.Object({
+				limit: t.Optional(t.String()),
+				additional: t.Optional(t.String()),
+			}),
+		}
+	)
--- src/app/api/player/index.ts
+++ src/app/api/player/index.ts
@@ -1,36 +1,37 @@
 import type { AxiosError } from 'axios'
-import { Elysia, t } from 'elysia'
+import { t } from 'elysia'
 import { apiClient } from '@/app/interceptors/sc.interceptor'
 import { type LotsResponse, Regions } from '@/types/api.type'
+import { createElysia } from '@/utils/elysia'
 
-export const playerRoute = new Elysia().get(
-    '/player/:region/:character/profile',
-    async ({ params, set }) => {
-        const { region, character } = params
+export const playerRoute = createElysia()
+    .get('/player/:region/:character/profile',
+        async ({ params, set }) => {
+            const { region, character } = params
 
-        try {
-            const { data } = await apiClient.get<LotsResponse>(
-                `/${region}/character/by-name/${character}/profile`,
-            )
+            try {
+                const { data } = await apiClient.get<LotsResponse>(
+                    `/${region}/character/by-name/${character}/profile`,
+                )
 
-            return data
-        } catch (err) {
-            const error = err as AxiosError<{ message?: string }>
-            const status = error.response?.status ?? 500
-            const message =
-                error.response?.data?.message ??
-                error.message ??
-                'Unknown error'
+                return data
+            } catch (err) {
+                const error = err as AxiosError<{ message?: string }>
+                const status = error.response?.status ?? 500
+                const message =
+                    error.response?.data?.message ??
+                    error.message ??
+                    'Unknown error'
 
-            set.status = status
+                set.status = status
 
-            return { error: message }
+                return { error: message }
+            }
+        },
+        {
+            params: t.Object({
+                character: t.String(),
+                region: t.Enum(Regions),
+            }),
         }
-    },
-    {
-        params: t.Object({
-            character: t.String(),
-            region: t.Enum(Regions),
-        }),
-    }
-)
+    )
