59e546c9cafbe5f66f16c88f0d8937b2ebd55d00
oarer <oarer.yml@gmail.com>
2026-04-24 16:51:48 +0500

    update entry index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,16 @@ import { app } from './app'
 import { env } from './env'
 import { createElysia } from './utils/elysia'
 
-const server = createElysia()
-  // Routes
+export const server = createElysia()
   .use(app)
 
   .get(
     "/",
     () =>
-      `Hi!\nThis API was created for https://stalhub.tech\n\nBy: https://github.com/oarer`
+      `Hi!\nThis API was created for https://stalhub.tech <3\n\nBy: https://github.com/oarer`
   )
+  .listen({ port: env.PORT }, ({ hostname, port }) => {
+    const url = env.NODE_ENV === 'production' ? 'https' : 'http'
 
-server.listen({ port: env.PORT }, ({ hostname, port }) => {
-  const url = env.NODE_ENV === 'production' ? 'https' : 'http'
-
-  console.log(`StalHub backend started on: ${url}://${hostname}:${port}`)
-})
+    console.log(`StalHub backend started on: ${url}://${hostname}:${port}`)
+  })
